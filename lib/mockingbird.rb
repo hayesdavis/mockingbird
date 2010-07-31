352f4429f4643aeb5f24ef05fc64fa88ad0fefdd
Hayes Davis <hayes@appozite.com>
2010-07-31 14:50:55 -0500

    Added version and documented convenience methods in Mockingbird module

--- lib/mockingbird.rb
+++ lib/mockingbird.rb
@@ -1,5 +1,9 @@
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/lib')
+# External dependencies
+require 'rubygems'
+require 'eventmachine'
 
+# Mockingbird code
+require 'mockingbird/version'
 require 'mockingbird/commands'
 require 'mockingbird/connection_script'
 require 'mockingbird/script'
@@ -10,6 +14,17 @@ module Mockingbird
   
   class << self
     
+    # Convenience method for starting a mockingbird server during a test. 
+    # This will be most users' primary interface to mockingbird. The mockingbird 
+    # server will be forked as a separate process. Ensure that your test code 
+    # always calls teardown at some point after setup, or the separate process 
+    # will not be terminated.
+    # 
+    # Options are
+    #   :host - The host to listen on. 0.0.0.0 by default
+    #   :port - The port to listen on. 4879 by default
+    #
+    # The block is a Mockingbird configuration (see README).
     def setup(opts={},&block)
       Server.configure(&block)
       @pid = fork do
@@ -19,9 +34,24 @@ module Mockingbird
       end
       Process.detach(@pid)
       puts "Waiting for Mockingbird to start..."
-      sleep(1)
+      sleep(1) # Necessary to make sure the forked proc is up and running
+      @pid
     end
     
+    # Terminates the mockingbird server created by a call to setup. Make sure 
+    # to always pair this call with setup to ensure that mockingbird server 
+    # processes don't linger. If you're using test/unit, the recommended 
+    # pattern is to actually call setup and teardown here during the setup and 
+    # teardown phase of your unit tests. Otherwise, use the following pattern:
+    # 
+    #   def test_something
+    #     Mockingbird.setup(:port=>NNNN) do
+    #       # config here
+    #     end
+    #     # do tests
+    #   ensure
+    #     Mockingbird.teardown
+    #   end
     def teardown
       Process.kill('KILL',@pid)
     end    
